8db5bef094a55383826103b2ab7443a00b9f78e9
Chris-Chris12345 <christian.amirza@outlook.com>
2024-10-13 15:25:34 +0200

    game.py

--- game.py
+++ game.py
@@ -16,6 +16,7 @@ score = 0
 move_sequence = 0
 move_counter = 0
 move_delay = 30
+initial_player_pos = (CENTER_X,700)
 
 def draw():
     screen.blit("background",(0,0))
@@ -39,7 +40,8 @@ def draw_lives():
         screen.blit("life",((i*35),10))
 
 def draw_aliens():
-    pass
+    for alien in aliens:
+        alien.draw()
 
 def draw_lasers():
     for laser in lasers:
@@ -47,6 +49,26 @@ def draw_lasers():
 
 def update():
     global move_counter, lasers 
+    if player.status < 30 and len(aliens) > 0:
+        check_keys()
+        update_lasers()
+        move_counter += 1
+        if move_counter == move_delay:
+            update_aliens()
+            move_counter = 0
+        if player.status > 0:
+            player.status += 1
+            if player.status == 30:
+                player.lives -= 1
+    else:
+        if keyboard.RETURN:
+            if player.lives > 0:
+                lasers = []
+                player.pos = initial_player_pos
+                player.status = 0
+            if player.lives == 0 and len(aliens) == 0:
+                init_game()
+
 
 def check_keys():
     global lasers
@@ -57,8 +79,8 @@ def check_keys():
         if player.x < 770:
             player.x += 5
     if keyboard.space:
-        if player_laser_active == 1:
-            player_laser_active = 0
+        if player.laser_active == 1:
+            player.laser_active = 0
             sounds.laser.play()
             clock.schedule(make_lasers_active, 1.0)
             l = len(lasers)
@@ -67,7 +89,7 @@ def check_keys():
             lasers[l].type = 1
 
 def make_lasers_active():
-    player_lasers_active = 1
+    player.lasers_active = 1
 
 def update_lasers():
     global lasers, aliens
@@ -83,10 +105,9 @@ def update_lasers():
             if laser.y < 5:
                 laser.status = 1
     
-    aliens = list_cleanup(aliens)
-    lasers = list_cleanup(lasers)
+    
 
-def list_cleanup(l):
+def list_clean_up(l):
     new_list = []
     for i in range(len[l]):
         if l[i] == 0:
@@ -110,7 +131,13 @@ def alien_laser_hit(laser):
 
 
 def init_aliens():
-    pass
+    global aliens
+    aliens = []
+    for i in range(18):
+        alien_x = 210 + (i%6)*80
+        alien_y = 100 + int(i/6)*64
+        aliens.append(Actor("alien1", (alien_x,alien_y)))
+        aliens[i].status = 0
 
 def update_aliens():
     global move_sequence, lasers
@@ -127,12 +154,12 @@ def update_aliens():
         animate(alien, pos = (alien.x + move_x, alien.y + move_y), duration = 0.5, tween = "linear")
 
         if randint(0,1) == 0:
-            alien.image("alien1")
+            alien.image="alien1"
         else:
-            alien.image("alien2")
+            alien.image="alien2"
             if randint(0,10) == 0:
                 l = len(lasers)
-                lasers.append("laser2",midtop = alien.midbotttom)
+                lasers.append(Actor("laser2",(alien.x, alien.y)))
                 lasers[l].status = 0
                 lasers[l].type = 0
         
@@ -142,6 +169,15 @@ def update_aliens():
 
 
 def init_game():
-    pass
+    global lasers
+    lasers = []
+    player.lives = 3
+    player.status = 0
+    player.pos = initial_player_pos
+    player.images = ["player","ex1","ex2","ex3","ex4","ex5"]
+    player.laser_active = 1
+    init_aliens()
 
+
+init_game()
 pgzrun.go()
